19f43f13c21fcab414c020b578514286e220091e
ito <kyosuke.ito@gmail.com>
2022-05-02 14:59:20 -0700

    basic apollo typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { useEffect } from "react";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+import { ExchangeRates } from "./ExchangeRates";
 
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
   cache: new InMemoryCache(),
 });
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
 function App() {
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          query GetRates {
-            rates(currency: "USD") {
-              currency
-            }
-          }
-        `,
-      })
-      .then((result) => console.log(result));
-  }, []);
   return (
-    <div>
-      <h1>Apollo Client</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Apollo Client</h1>
+        <ExchangeRates exchangeRates={EXCHANGE_RATES} />
+      </div>
+    </ApolloProvider>
   );
 }
 export default App;
